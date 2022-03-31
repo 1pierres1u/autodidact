23a817afecc3b2507f3de5c71b3f3d4fafe4de4f
Pierre S Louis-Jacques <1pierres1u@gmail.com>
2022-03-31 10:51:38 -0400

    Processor architecture, processor instruction

---
+++ cmake/03_31_2022/processor_instruction/build/config.h
@@ -0,0 +1,20 @@
+#pragma once
+#define NUMBER_OF_LOGICAL_CORES 12
+#define NUMBER_OF_PHYSICAL_CORES 6
+#define TOTAL_VIRTUAL_MEMORY 10984
+#define AVAILABLE_VIRTUAL_MEMORY 2093
+#define TOTAL_PHYSICAL_MEMORY 7521
+#define AVAILABLE_PHYSICAL_MEMORY 2933
+#define IS_64BIT 1
+#define HAS_FPU 1
+#define HAS_MMX_PLUS 1
+#define HAS_SSE 1
+#define HAS_SSE2 1
+#define HAS_SSE_FP 0
+#define HAS_AMD_3DNOW 0
+#define HAS_AMD_3DNOW_PLUS 0
+#define HAS_IA64 0
+#define OS_NAME "Windows"
+#define OS_RELEASE " Personal"
+#define OS_VERSION " (Build 22000)"
+#define OS_PLATFORM "AMD64"
